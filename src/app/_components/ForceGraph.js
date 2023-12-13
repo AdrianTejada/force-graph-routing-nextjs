07f165919a2f944053c94653d430ac8eaa805995
AdrianTejada <atejada@my.bcit.ca>
2023-12-12 16:35:13 -0800

    Tutorial branch ready

--- src/app/_components/ForceGraph.js
+++ src/app/_components/ForceGraph.js
@@ -1,9 +1,17 @@
-'use client'
+"use client";
+import { useRouter } from "next/navigation";
 
-export default function ForceGraph() {
+export default function ForceGraph({ width = 250, height = 250 }) {
+  const router = useRouter();
+
+  const handleRoute = (route) => {
+    router.push(route);
+  };
+
   return (
-    <svg className="w-[250px] h-[250px] bg-white drop-shadow-lg rounded-md">
-
-    </svg>
-  )
+    <svg
+      className={`w-[${width}px] h-[${height}px] bg-white drop-shadow-lg rounded-md`}
+      viewBox={`${-width / 2}, ${-height / 2}, ${width}, ${height}`}
+    ></svg>
+  );
 }
--- src/app/_components/Navbar.js
+++ src/app/_components/Navbar.js
@@ -1,7 +1,7 @@
 'use client'
 import { usePathname } from "next/navigation"
 import Link from "next/link"
-import { pages } from "@/data/NavbarData";
+import nodes from "@/data/nodes";
 
 export default function Navbar() {
   const currentPath = usePathname();
@@ -9,16 +9,16 @@ export default function Navbar() {
   return (
     <header className="my-5">
       <nav className="flex flex-row gap-x-3">
-        {pages.map((page) => (
+        {nodes.map((node) => (
           <Link 
-            key={page.path}
-            href={page.path}
+            key={node.id}
+            href={node.route}
             className={`
               tracking-wider
-              ${currentPath === page.path ? 'text-blue-600' : 'text-black'}
+              ${currentPath === node.route ? 'text-blue-600' : 'text-black'}
             `}
           >
-            {page.name}  
+            {node.id}
           </Link>
         ))}
       </nav>
